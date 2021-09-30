4acb92f3f553b5fdf924685a03c36db17c95fbb9
maria <marianmartorell.93@gmail.com>
2021-09-30 10:38:18 +0200

    Initial commit

---
+++ src/com/Marian/Exercici2/Persona.java
@@ -0,0 +1,77 @@
+package com.Marian.Exercici2;
+
+public class Persona {
+    private String name;
+    private int age;
+    private String dni;
+    private char sex;
+    private int phone;
+
+    // contructors
+
+    public Persona(){};
+
+    public Persona(String name, int age, String dni, char sex, int phone){
+
+        this.name = name;
+        this.age = age;
+        this.dni = dni;
+        this.sex = sex;
+        this.phone = phone;
+    }
+
+    //getters
+
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getDni(){
+        return this.dni;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    //setters
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    //display
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", dni='" + dni + '\'' +
+                ", sex=" + sex +
+                ", phone=" + phone +
+                '}';
+    }
+
+}
---
+++ src/com/Marian/Main.java
@@ -0,0 +1,36 @@
+package com.Marian;
+
+import com.Marian.Exercici2.Persona;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona persona1 = new Persona("Maria",20,"23456755M",'D',677543124);
+
+        System.out.println(persona1.toString());
+
+        Persona persona2 = new Persona();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        persona2.setName(sc.nextLine());
+
+        System.out.print("Age: ");
+        persona2.setAge(sc.nextInt());
+
+        System.out.print("DNI: ");
+        persona2.setDni(sc.nextLine());
+
+        System.out.print("Sex: ");
+        persona2.setSex(sc.nextLine());
+
+        System.out.print("");
+        persona2.setPhone(667488326);
+
+        System.out.println();
+        System.out.println(persona2.toString());
+    }
+}
